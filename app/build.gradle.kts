80703457bf48c5ebeec912f19dc05943b40eeacb
ValenteAlvarez <valentealvarez1@gmail.com>
2024-11-05 22:43:41 -0600

    Agregar librerias, dependencias y permisos en Gradle y AndroidManifest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,15 @@ android {
 
 dependencies {
 
+    // Dependencias para camara, foto y video
+    implementation("androidx.camera:camera-core:1.4.0")
+    implementation("androidx.camera:camera-lifecycle:1.4.0")
+    implementation("androidx.camera:camera-camera2:1.4.0")
+    implementation("androidx.camera:camera-extensions:1.4.0")
+    implementation("androidx.camera:camera-video:1.4.0")
+    implementation("androidx.camera:camera-view:1.4.0")
+
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
